5e7df999d1c28d627cf748065b3783287da8ae90
Jonas Kohler <jonaskohler@staff-net-cx-0093.ethz.ch>
2020-06-25 11:47:25 +0200

    test: added noise to depict individual vertices

--- tests/transition_graph_test.py
+++ tests/transition_graph_test.py
@@ -16,18 +16,19 @@ from flatland.utils import rendertools
 
 from youtube_flatland.environment import transition_graph
 
-
 class EnvTest(absltest.TestCase):
     
     def test_build_graph(self):
         env = rail_env.RailEnv(
-                width=5,
-                height=5,
+                width=10,
+                height=10,
                 )
         obs, info = env.reset()
         g = transition_graph.build_transition_graph(env)
         plt = igraph.Plot()
-        layout = [(v['y'], v['x']) for v in g.vs]
+
+        layout = [(v['y']+np.random.randn()*0.075, v['x']+np.random.randn()*0.075) for v in g.vs]
+
         plt.add(g, layout=layout)
         plt.redraw()
         with tempfile.NamedTemporaryFile() as f:
@@ -40,6 +41,7 @@ class EnvTest(absltest.TestCase):
         graph_img = graph_img.resize(bg_img.size)
         h, w = bg_img.size
         bg_img = bg_img.resize((h//5*6, w//5*6))
+
         bg_img.paste(graph_img, (h//12, w//12), graph_img)
         bg_img.show()
 
