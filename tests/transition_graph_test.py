c46b0cd956b28f3373f7c781f07cbb86377a926c
Jonas Kohler <jonaskohler@staff-net-cx-0093.ethz.ch>
2020-06-25 16:37:13 +0200

    graph: removing linear paths possible IF start is given correctly. TODO: remove all linear paths

--- tests/transition_graph_test.py
+++ tests/transition_graph_test.py
@@ -12,6 +12,9 @@ from PIL import Image
 
 from flatland.envs import rail_env
 from flatland.envs import rail_generators
+
+
+
 from flatland.utils import rendertools
 
 from youtube_flatland.environment import transition_graph
@@ -20,15 +23,28 @@ class EnvTest(absltest.TestCase):
     
     def test_build_graph(self):
         env = rail_env.RailEnv(
-                width=10,
-                height=10,
-                )
+                width=25,
+                height=15,
+                rail_generator=rail_generators.sparse_rail_generator())
+
         obs, info = env.reset()
         g = transition_graph.build_transition_graph(env)
-        plt = igraph.Plot()
 
+        #delete two paths as an example:
+        delete_stuff=True
+        if delete_stuff: 
+            path_1=transition_graph.get_linear_path(g,g.vs[93]) 
+            path_2=transition_graph.get_linear_path(g,g.vs[25]) 
+
+        vs_to_delete=transition_graph.join_linear_path(g,path_1)      
+        vs_to_delete+=transition_graph.join_linear_path(g,path_2)   
+
+        g.delete_vertices(vs_to_delete)
+
+        plt = igraph.Plot()
         layout = [(v['y']+np.random.randn()*0.075, v['x']+np.random.randn()*0.075) for v in g.vs]
 
+    
         plt.add(g, layout=layout)
         plt.redraw()
         with tempfile.NamedTemporaryFile() as f:
--- youtube_flatland/environment/transition_graph.py
+++ youtube_flatland/environment/transition_graph.py
@@ -25,15 +25,42 @@ def build_transition_graph(env):
     for x in range(env.height):
         for y in range(env.width):
             for o in Direction:
-                k = (x, y, o)
+                k = (x, y, o) #x,y pos as well as valid in-orientations
                 v = nodes[k]
                 for t in Direction:
                     if env.rail.get_transition(k, t):
                         newx, newy = modify_coordinates(x, y, t)
                         v_dest = nodes[(newx, newy, t)]
                         g.add_edge(v, v_dest)
+                        g.es[-1]['length']=1
     for k, v in nodes.items():
         v['x'], v['y'], v['o'] = k
     empty_vs = [v.index for v in g.vs if not v.all_edges()]
     g.delete_vertices(empty_vs)
+
     return g
+
+
+def get_linear_path(g,v): #only explores one direction-> start right after junction
+    visited=[v]
+    while True:
+        if len(v.successors())!=1 or is_dead_end(v.successors()[0]) or is_junction(g,v.successors()[0]) or v.successors()[0] in visited: #switch or dead-end or junction or loop round
+            break
+        v=v.successors()[0]
+        visited.append(v)
+    return visited
+
+
+def join_linear_path(g,path):
+    assert len(path)>2, "only paths longer than two can be joined"
+    g.add_edge(path[0],path[-1],length=len(path)-1)
+    return [v.index for v in path[1:-1]] 
+   
+def is_junction(g,v):    
+    x_matches=[i for i, e in enumerate(g.vs['x']) if e == v['x']]
+    matches=list(g.vs[x_matches]['y']).count(v['y'])
+    return matches>2
+
+def is_dead_end(v):
+    return len(v.successors())==1 and abs(v['o'] - v.successors()[0]['o'])==2
+
